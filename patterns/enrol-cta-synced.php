bddaece9ad4ee11061fda3a8432f5efe2dae7f4f
Luana Bastos <lu.bastos13@gmail.com>
2025-10-17 11:41:57 -0700

    added archive template

---
+++ patterns/enrol-cta-synced.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Enrol CTA Synced
+ * Slug: school-site-theme/enrol-cta-synced
+ * Categories: Call to action
+ */
+?>
+<!-- wp:cover {"url":"http://localhost/school-site/wp-content/uploads/2025/10/coding-workshop-1024x683.jpg","alt":"People working on laptops.","dimRatio":40,"overlayColor":"custom-color-6","isUserOverlayColor":true,"minHeight":600,"tagName":"section","sizeSlug":"large","metadata":{"name":"CTA"},"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"600px"}} -->
+<section class="wp-block-cover" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;min-height:600px"><img class="wp-block-cover__image-background size-large" alt="People working on laptops." src="http://localhost/school-site/wp-content/uploads/2025/10/coding-workshop-1024x683.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-custom-color-6-background-color has-background-dim-40 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":"1.3"}},"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-x-large-font-size" style="line-height:1.3">Education Today, Success Tomorrow!</h2>
+<!-- /wp:heading -->
+
+<!-- wp:school-site-blocks/aos-block {"effect":"flip-left"} -->
+<div data-aos="flip-left" class="wp-block-school-site-blocks-aos-block"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","width":50} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-50"><a class="wp-block-button__link has-text-align-center wp-element-button" href="http://localhost/school-site/application-form/">Enrol Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:school-site-blocks/aos-block --></div></section>
+<!-- /wp:cover -->
